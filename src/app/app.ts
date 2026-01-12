5d4d461d1f85c7fe26c37e44e8ca88ad905377f3
Artem Sokolov <sknd2569@gmail.com>
2026-01-12 14:54:36 +0100

    added tailwind, angular material, eslint

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
 })
+
 export class App {
-  protected readonly title = signal('pilot-briefing');
+  protected readonly title = signal('Pilot Briefing App')
 }
