01cc36876982841bde60ef601b738fc0c66275fb
Artem Sokolov <sknd2569@gmail.com>
2026-01-14 22:04:28 +0100

    initiated table with dummy data

--- src/app/briefing-table/briefing-table.ts
+++ src/app/briefing-table/briefing-table.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core'
+import { MatTableDataSource, MatTableModule } from '@angular/material/table'
+
+const DATA = [
+  { position: 1, name: 'Hydrogen' },
+  { position: 2, name: 'Helium' },
+  { position: 3, name: 'Lithium' },
+  { position: 4, name: 'Beryllium' },
+  { position: 5, name: 'Boron' },
+  { position: 6, name: 'Carbon' },
+  { position: 7, name: 'Nitrogen' },
+  { position: 8, name: 'Oxygen' },
+  { position: 9, name: 'Fluorine' },
+  { position: 10, name: 'Neon' },
+]
 
 @Component({
   selector: 'app-briefing-table',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './briefing-table.html',
 })
 export class BriefingTable {
-
+  displayedColumns: string[] = ['position', 'name']
+  dataSource = new MatTableDataSource(DATA)
 }
